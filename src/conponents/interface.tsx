623d2be5dd82750e25a65186ca531148121306dd
PhanVoAnhKiet <phanvoanhkiet1389@gmail.com>
2023-06-23 16:50:37 +0700

    A

---
+++ src/conponents/interface.tsx
@@ -0,0 +1,10 @@
+export interface BaseUser {
+    name: string;
+    address: string;
+    age:  string;
+    profession: string;
+    interestRate: string
+  }
+  export interface User extends BaseUser {
+    id: number;
+  }
